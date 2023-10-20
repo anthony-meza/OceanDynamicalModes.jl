36fa074e5e3fbff0906c0210c3462491f06156d0
Documenter.jl <documenter@juliadocs.github.io>
2023-10-20 20:41:21 +0000

    build based on 25fa7a9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OceanDynamicalModes","category":"page"},{"location":"#OceanDynamicalModes","page":"Home","title":"OceanDynamicalModes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OceanDynamicalModes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OceanDynamicalModes]","category":"page"}]
+}
