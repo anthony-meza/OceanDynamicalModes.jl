eebbcf02a97a504f074e0452e54bc8ca8a49a43f
anthony-meza <64243783+anthony-meza@users.noreply.github.com>
2023-10-23 00:27:04 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,4 @@ using Test
 
 @testset "OceanDynamicalModes.jl" begin
     # Write your tests here.
-    dz = 0.05
-    depth = collect(0 + dz:dz:1. - 0.05)
-    Nsq = ones(length(depth))
-    nmodes = 3
-    wmodes, pmodes, ce = dynmodes(Nsq, depth, dz, nmodes)
 end
